01db8eae36b7a29787585a3cb797db2f2a4d03e0
cyberplantru <cyberplant.ru@gmail.com>
2015-12-06 13:21:03 +0300

    master

--- PhMeasurementSerial/PhMeasurementSerial.ino
+++ PhMeasurementSerial/PhMeasurementSerial.ino
@@ -2,21 +2,21 @@
   Example code for the pHtoI2C v2.0
   
   http://www.cyberplant.info
-  by CyberPlant LLC, 14 November 2015
+  by CyberPlant LLC
   This example code is in the public domain.
-
+  upd. 06.12.2015
 */
 
 #include "Wire.h"
 #include <EEPROM.h>
 #define pHtoI2C 0x48
+#define T 273.15 // degrees Kelvin
 
-
-int addressCentrePoint = 0;
-int addressAlphaL = addressCentrePoint+sizeof(float);
+int addressIsoP = 0;
+int addressAlphaL = addressIsoP+sizeof(float);
 int addressAlphaH = addressAlphaL+sizeof(float);
 
-float CentrePoint = EEPROM_float_read(addressCentrePoint);
+float IsoP = EEPROM_float_read(addressIsoP);
 float alphaL = EEPROM_float_read(addressAlphaL);
 float alphaH = EEPROM_float_read(addressAlphaH);
 
@@ -24,14 +24,7 @@ float voltage, data;
 byte highbyte, lowbyte, configRegister;
 
 float pH;
-float temp = 25.0; // *C
-
-const int numReadings = 10;
-
-float readings[numReadings];      // the readings from the analog input
-int index = 0;                  // the index of the current reading
-float total = 0;                  // the running total
-float average = 0;                // the average
+float tempManual= 25.0;
 
 unsigned int Interval = 1000;
 long previousMillis = 0;
@@ -45,14 +38,22 @@ void setup()
   Wire.begin();
   Serial.begin(9600);
   Time=millis();
+  
+if (IsoP < 1 || IsoP > 10) // Primary auto setting
+{
+  Reset_pH();
+}
 
-  for (int thisReading = 0; thisReading < numReadings; thisReading++)
-readings[thisReading] = 0;
 
-  Serial.println("Send command for: Reset > R || Cal. pH 4.00 > 4 || Cal. pH 7.00 > 7 || Cal. pH 10.00 > 9");
+  Serial.println("Calibrate commands:");
+  Serial.println("pH :");
+  Serial.println("      Cal. pH 4.00 ---- 4");
+  Serial.println("      Cal. pH 7.00 ---- 7");
+  Serial.println("      Cal. pH 10.00 --- 9");
+  Serial.println("      Reset pH ---------8");
+  Serial.println("  ");
   delay(250);
-  Serial.println("______________________");
-  }
+}
   
   void loop()
 {
@@ -60,58 +61,29 @@ readings[thisReading] = 0;
 if (millis()-Time>=Interval)
 {  
   Time = millis();
-  pH = pHread();
-  Serial.print("pH ");
+  
+  pH_read();
+  pH_calculate();
+  
+  Serial.println("  ");
+  //Serial.print("t ");
+  //Serial.print(tempManualManual);
+  //Serial.print(" *C");
+  Serial.print("    pH ");
   Serial.println(pH);
-  Serial.println("______________________");
 }
 
-    if (Serial.available() > 0) //  function of calibration pH
+       if (Serial.available() > 0) //  function of calibration E.C.
     {  
-      
         incomingByte = Serial.read();
-        
- if (incomingByte == 114) // press key "R"
- {
-  Serial.println("Reset Settings...");
-  EEPROM_float_write(addressCentrePoint, 7.00);
-  EEPROM_float_write(addressAlphaL, -59.16);
-  EEPROM_float_write(addressAlphaH, -59.16);
-  alphaL = EEPROM_float_read(addressAlphaL);
-  alphaH = EEPROM_float_read(addressAlphaH);
-  CentrePoint = EEPROM_float_read(addressCentrePoint);  
-  Serial.println("Complete");
-  Serial.println("______________________");
- }
- if (incomingByte == 52) // press key "4"
- {
-  Serial.println("Cal. pH 4.00");
-  alphaL = ((4.00 - CentrePoint) * (273.15 + temp) / voltage); 
-  EEPROM_float_write(addressAlphaL, alphaL);
-  Serial.println("Settings saved");
-  Serial.println("______________________");
- }
- if (incomingByte == 55) // press key "7"
- {
-  Serial.println("Cal. pH 7.00");
-  CentrePoint = (CentrePoint - pH + 7.00);
-  EEPROM_float_write(addressCentrePoint, CentrePoint);
-  Serial.println("Settings saved");
-  Serial.println("______________________");
- }
-  if (incomingByte == 57) // press key "9"
- {
-  Serial.println("Cal. pH 10.00");
-  alphaH = ((10.00 - CentrePoint) * (273.15 + temp) / voltage); 
-  EEPROM_float_write(addressAlphaH, alphaH);
-  Serial.println("Settings saved");
-  Serial.println("______________________");
- }
+        cal_sensors();
     }
-
+        
 }
 
-float ADSread() // read ADS
+/*-----------------------------------End loop---------------------------------------*/ 
+
+float pH_read() // read ADS
 {
   Wire.requestFrom(pHtoI2C, 3);
   while(Wire.available()) // ensure all the data comes in
@@ -123,26 +95,59 @@ float ADSread() // read ADS
   data = highbyte * 256;
   data = data + lowbyte;
   voltage = data * 2.048 ;
-  voltage = voltage / 327.68; // mV
+  voltage = voltage / 32768;
   return voltage;
 }
 
-float pHread() // calculate pH
+float pH_calculate() // calculate pH
 {
-    voltage = ADSread();
-    total= total - readings[index];
-    if (voltage > 0)
-    readings[index] = ((alphaL * voltage / (273.15 + temp)) + CentrePoint);
-    else if (voltage < 0)
-    readings[index] = ((alphaH * voltage / (273.15 + temp)) + CentrePoint);
-    total= total + readings[index];
-    index = index + 1;
-    // if we're at the end of the array...
-    if (index >= numReadings)              
-    // ...wrap around to the beginning: 
-    index = 0;    
-    average = total / numReadings;
-    pH = average;
+  if (voltage > 0)
+  pH = 7.50 - alphaL * (T + tempManual) * voltage;
+  else if (voltage < 0)
+  pH = 7.50 - alphaH * (T + tempManual) * voltage;
+  return pH;
+}
+
+void cal_sensors()
+{
+  Serial.println(" ");
+  if (incomingByte == 56) // press key "8"
+ {
+  Reset_pH();
+ }
+ else if (incomingByte == 52) // press key "4"
+ {
+  Serial.print("Cal. pH 4.00 ...");
+  alphaL = (IsoP - 4) / voltage / (T + tempManual); 
+  EEPROM_float_write(addressAlphaL, alphaL);
+  Serial.println(" complete");
+ }
+ else if (incomingByte == 55) // press key "7"
+ {
+  Serial.print("Cal. pH 7.00 ...");
+  IsoP = (IsoP - pH + 7.00);
+  EEPROM_float_write(addressIsoP, IsoP);
+  Serial.println(" complete");
+ }
+  else if (incomingByte == 57) // press key "9"
+ {
+  Serial.print("Cal. pH 10.00 ...");
+  alphaH = (IsoP - 10) / voltage / (T + tempManual); 
+  EEPROM_float_write(addressAlphaH, alphaH);
+  Serial.println(" complete");
+ }
+}
+
+void Reset_pH()
+{
+  Serial.print("Reset pH ...");
+  EEPROM_float_write(addressIsoP, 7.5099949836);
+  EEPROM_float_write(addressAlphaL, 0.0778344535);
+  EEPROM_float_write(addressAlphaH, 0.0850976657);
+  alphaL = EEPROM_float_read(addressAlphaL);
+  alphaH = EEPROM_float_read(addressAlphaH);
+  IsoP = EEPROM_float_read(addressIsoP);  
+  Serial.println(" complete");
 }
 
 void EEPROM_float_write(int addr, float val) // write to EEPROM
@@ -158,3 +163,4 @@ float EEPROM_float_read(int addr) // read of EEPROM
   float *y = (float *)&x;
   return y[0];
 }
+
