1e92a47d631284f0a238bffdba06586031c50370
cyberplantru <cyberplant.ru@gmail.com>
2015-12-08 12:13:39 +0300

    master
    for Arduino IDE 1.6.6

--- PhAndTempMeasurementSerial/PhAndTempMeasurementSerial.ino
+++ PhAndTempMeasurementSerial/PhAndTempMeasurementSerial.ino
@@ -2,9 +2,10 @@
   Example code for the pH to I2C module v2.0
   
   http://www.cyberplant.info
-  by CyberPlant LLC
+  by CyberPlant LLC, 14 November 2015
   This example code is in the public domain.
-  upd. 06.12.2015
+  
+  upd. 08.12.2015
 
 */
 
@@ -15,14 +16,6 @@
 #define PT_TC               0.3850  
 #define PT_RES_NOMINAL      100.781  // +- 0.39 for pt100  
 #define T 273.15                    // degrees Kelvin
-
-int addressIsoP = 0;
-int addressAlphaL = addressIsoP+sizeof(float);
-int addressAlphaH = addressAlphaL+sizeof(float);
-
-float IsoP = EEPROM_float_read(addressIsoP);
-float alphaL = EEPROM_float_read(addressAlphaL);
-float alphaH = EEPROM_float_read(addressAlphaH);
 
 float voltage, data;
 byte highbyte, lowbyte, configRegister;
@@ -49,6 +42,16 @@ int sequence = 0;
 
 enum { REG = 10 }; // pin D10 is SS line for LMP91200
 
+  float IsoP;
+  float AlphaL;
+  float AlphaH;
+  
+ struct MyObject {
+  float IsoP;
+  float AlphaL;
+  float AlphaH;
+};
+
 void writeLMP91200(int ss_pin, uint16_t value)
 {
   digitalWrite(ss_pin, LOW);
@@ -69,6 +72,15 @@ void setup()
   pinMode(REG, OUTPUT);
   writeLMP91200(REG, 0x0000); // read pH
 
+  int eeAddress = 0; 
+
+  MyObject customVar;
+  EEPROM.get(eeAddress, customVar);
+  
+  IsoP = (customVar.IsoP);
+  AlphaL = (customVar.AlphaL);
+  AlphaH = (customVar.AlphaH);
+  
   Time=millis();
 
   for (int thisReadingtemp = 0; thisReadingtemp < numReadingstemp; thisReadingtemp++)
@@ -88,7 +100,127 @@ void setup()
   Serial.println("  ");
   delay(250);
 }
+
+void PT_CAL_read()
+{
+  ADSread();
+  PT_CAL = voltage;
+}
+
+void pt100_read()
+{
+  ADSread();
+  totaltemp= totaltemp - readingstemp[indextemp];
+  readingstemp[indextemp] = ((voltage/PT_CAL)* PT_CAL - PT_RES_NOMINAL) / PT_TC;
+  totaltemp= totaltemp + readingstemp[indextemp];
+  indextemp = indextemp + 1;
+  // if we're at the end of the array...
+  if (indextemp >= numReadingstemp)              
+  // ...wrap around to the beginning: 
+  indextemp = 0;    
+  averagetemp = totaltemp / numReadingstemp;
+  temp = averagetemp;
   
+  if (temp>200 || temp<-20)
+    {
+    temp = tempManual;
+    }  
+}
+
+void ADSread() // read ADS
+{
+  Wire.requestFrom(pHtoI2C, 3);
+  while(Wire.available()) // ensure all the data comes in
+  {
+    highbyte = Wire.read(); // high byte * B11111111
+    lowbyte = Wire.read(); // low byte
+    configRegister = Wire.read();
+  }
+  data = highbyte * 256;
+  data = data + lowbyte;
+  voltage = data * 2.048 ;
+  voltage = voltage / 327.68; // mV
+}
+
+void pH_read() // read ADS
+{
+  Wire.requestFrom(pHtoI2C, 3);
+  while(Wire.available())
+  {
+    highbyte = Wire.read();
+    lowbyte = Wire.read();
+    configRegister = Wire.read();
+  }
+  data = highbyte * 256;
+  data = data + lowbyte;
+  voltage = data * 2.048 ;
+  voltage = voltage / 32768;
+}
+
+void pH_calculate() // calculate pH
+{
+  if (voltage > 0)
+  pH = IsoP - AlphaL * (T + tempManual) * voltage;
+  else if (voltage < 0)
+  pH = IsoP - AlphaH * (T + tempManual) * voltage;
+}
+
+void cal_sensors()
+{
+ Serial.println(" ");
+ if (incomingByte == 56) // press key "8"
+ {
+  Reset_pH();
+ }
+ else if (incomingByte == 52) // press key "4"
+ {
+  Serial.print("Cal. pH 4.00 ...");
+  AlphaL = (IsoP - 4) / voltage / (T + tempManual);
+  int eeAddress = 0 + sizeof(float);
+  EEPROM.put(eeAddress,  AlphaL);
+  Serial.println(" complete");
+ }
+ else if (incomingByte == 55) // press key "7"
+ {
+  Serial.print("Cal. pH 7.00 ...");
+  IsoP = (IsoP - pH + 7.00);
+  int eeAddress = 0;
+  EEPROM.put(eeAddress, IsoP);
+  Serial.println(" complete");
+ }
+  else if (incomingByte == 57) // press key "9"
+ {
+  Serial.print("Cal. pH 10.00 ...");
+  AlphaH = (IsoP - 10) / voltage / (T + tempManual); 
+  int eeAddress = 0 + (sizeof(float)*2); 
+  EEPROM.put(eeAddress,  AlphaH);
+  Serial.println(" complete");
+ }
+}
+
+void Reset_pH()
+{
+  int eeAddress = 0;
+
+  Serial.print("Reset pH ...");
+
+  MyObject customVar = {
+    7.5099949836,
+    0.0778344535,
+    0.0850976657
+  };
+
+  EEPROM.put(eeAddress, customVar);
+
+  EEPROM.get(eeAddress, customVar);
+  
+  IsoP = (customVar.IsoP);
+  AlphaL = (customVar.AlphaL);
+  AlphaH = (customVar.AlphaH);
+  Serial.println(" complete");
+}
+
+
   void loop()
 {
 if (millis()-Time>=Interval)
@@ -120,17 +252,14 @@ if (millis()-Time>=Interval)
   {
     pH_read();
     pH_calculate();
-    pH25_calculate();
   Serial.println("  ");  
   Serial.print("  Temp ");
   Serial.print(temp, 2);
   Serial.print(" *C ");
   Serial.print("  pH ");
   Serial.print(pH);
-  Serial.print("  pH25 ");
-  Serial.println(pH25);
-  //Serial.print("  R_CAL ");
-  //Serial.println(PT_CAL);
+  Serial.print("  R_CAL ");
+  Serial.println(PT_CAL);
   }
 }
 
@@ -144,137 +273,4 @@ if (millis()-Time>=Interval)
 
 /*-----------------------------------End loop---------------------------------------*/ 
 
-float PT_CAL_read()
-{
-  ADSread();
-  PT_CAL = voltage;
-  return PT_CAL;
-}
 
-float pt100_read()
-{
-  ADSread();
-  totaltemp= totaltemp - readingstemp[indextemp];
-  readingstemp[indextemp] = ((voltage/PT_CAL)* PT_CAL - PT_RES_NOMINAL) / PT_TC;
-  totaltemp= totaltemp + readingstemp[indextemp];
-  indextemp = indextemp + 1;
-  // if we're at the end of the array...
-  if (indextemp >= numReadingstemp)              
-  // ...wrap around to the beginning: 
-  indextemp = 0;    
-  averagetemp = totaltemp / numReadingstemp;
-  temp = averagetemp;
-  
-  if (temp>200 || temp<-20)
-    {
-    temp = tempManual;
-    }
-    
-  return temp;
-}
-
-float ADSread() // read ADS
-{
-  Wire.requestFrom(pHtoI2C, 3);
-  while(Wire.available()) // ensure all the data comes in
-  {
-    highbyte = Wire.read(); // high byte * B11111111
-    lowbyte = Wire.read(); // low byte
-    configRegister = Wire.read();
-  }
-  data = highbyte * 256;
-  data = data + lowbyte;
-  voltage = data * 2.048 ;
-  voltage = voltage / 327.68; // mV
-  return voltage;
-}
-
-float pH_read() // read ADS
-{
-  Wire.requestFrom(pHtoI2C, 3);
-  while(Wire.available()) // ensure all the data comes in
-  {
-    highbyte = Wire.read(); // high byte * B11111111
-    lowbyte = Wire.read(); // low byte
-    configRegister = Wire.read();
-  }
-  data = highbyte * 256;
-  data = data + lowbyte;
-  voltage = data * 2.048 ;
-  voltage = voltage / 32768;
-  return voltage;
-}
-
-float pH_calculate() // calculate pH
-{
-  if (voltage > 0)
-  pH = IsoP - alphaL * (T + temp) * voltage;
-  else if (voltage < 0)
-  pH = IsoP - alphaH * (T + temp) * voltage;
-  return pH;
-}
-
-float pH25_calculate() // calculate pH
-{
-  if (voltage > 0)
-  pH25 = IsoP - alphaL * (T + tempManual) * voltage;
-  else if (voltage < 0)
-  pH25 = IsoP - alphaH * (T + tempManual) * voltage;
-  return pH25;
-}
-
-void cal_sensors()
-{
-  Serial.println("  ");  
-  if (incomingByte == 56) // press key "8"
- {
-  Reset_pH();
- }
- else if (incomingByte == 52) // press key "4"
- {
-  Serial.print("Cal. pH 4.00 ...");
-  alphaL = (IsoP - 4) / voltage / (T + temp); 
-  EEPROM_float_write(addressAlphaL, alphaL);
-  Serial.println(" complete");
- }
- else if (incomingByte == 55) // press key "7"
- {
-  Serial.print("Cal. pH 7.00 ...");
-  IsoP = (IsoP - pH + 7.00);
-  EEPROM_float_write(addressIsoP, IsoP);
-  Serial.println(" complete");
- }
-  else if (incomingByte == 57) // press key "9"
- {
-  Serial.print("Cal. pH 10.00 ...");
-  alphaH = (IsoP - 10) / voltage / (T + temp); 
-  EEPROM_float_write(addressAlphaH, alphaH);
-  Serial.println(" complete");
- }
-}
-
-void Reset_pH()
-{
-  Serial.print("Reset pH ...");
-  EEPROM_float_write(addressIsoP, 7.5099949836);
-  EEPROM_float_write(addressAlphaL, 0.0778344535);
-  EEPROM_float_write(addressAlphaH, 0.0850976657);
-  alphaL = EEPROM_float_read(addressAlphaL);
-  alphaH = EEPROM_float_read(addressAlphaH);
-  IsoP = EEPROM_float_read(addressIsoP);  
-  Serial.println(" complete");
-}
-
-void EEPROM_float_write(int addr, float val) // write to EEPROM
-{  
-  byte *x = (byte *)&val;
-  for(byte i = 0; i < 4; i++) EEPROM.write(i+addr, x[i]);
-}
-
-float EEPROM_float_read(int addr) // read of EEPROM
-{    
-  byte x[4];
-  for(byte i = 0; i < 4; i++) x[i] = EEPROM.read(i+addr);
-  float *y = (float *)&x;
-  return y[0];
-}
